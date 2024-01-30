455d7a722e1e38297cc4067dbc2fee466419735b
Alex Curran <alexcurran.va@gmail.com>
2024-01-30 16:52:01 -0600

    Add newline to separate built-in Node modules from installed modules

--- db/index.js
+++ db/index.js
@@ -1,7 +1,8 @@
 "use strict";
 
-const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
+
+const sqlite3 = require("sqlite3").verbose();
 // always keep this db file within the db folder (use path.join() method)
 const db = new sqlite3.Database(path.join(__dirname, "books_db.db"));
 
