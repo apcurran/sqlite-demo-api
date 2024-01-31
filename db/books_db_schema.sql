2bfaec5b8cf7a762967f9228e702981bdebddf76
Alex Curran <alexcurran.va@gmail.com>
2024-01-31 16:19:30 -0600

    genre and book_genre tables added to schema

--- db/books_db_schema.sql
+++ db/books_db_schema.sql
@@ -12,3 +12,16 @@ CREATE TABLE author(
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL
 );
+
+CREATE TABLE genre(
+    genre_id INTEGER PRIMARY KEY,
+    genre_name TEXT NOT NULL
+);
+
+-- junction table connecting book with genre
+CREATE TABLE book_genre(
+    book_id INTEGER NOT NULL,
+    FOREIGN KEY(book_id) REFERENCES book(book_id),
+    genre_id INTEGER NOT NULL,
+    FOREIGN KEY(genre_id) REFERENCES genre(genre_id)
+);
