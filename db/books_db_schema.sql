05f1b91fbe87a7ada8d21d70707dae3ead777bfd
Alex Curran <alexcurran.va@gmail.com>
2024-01-31 16:33:10 -0600

    Rename and move order of FOREIGN KEY constraint statements for SQLite to not throw an error

--- db/books_db_schema.sql
+++ db/books_db_schema.sql
@@ -1,27 +1,27 @@
-CREATE TABLE book(
+CREATE TABLE book (
     book_id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     year INTEGER NOT NULL,
     pages INTEGER NOT NULL,
     author_id INTEGER NOT NULL,
-    FOREIGN KEY(author_id) REFERENCES author(author_id)
+    FOREIGN KEY (author_id) REFERENCES author (author_id)
 );
 
-CREATE TABLE author(
+CREATE TABLE author (
     author_id INTEGER PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL
 );
 
-CREATE TABLE genre(
+CREATE TABLE genre (
     genre_id INTEGER PRIMARY KEY,
     genre_name TEXT NOT NULL
 );
 
 -- junction table connecting book with genre
-CREATE TABLE book_genre(
-    book_id INTEGER NOT NULL,
-    FOREIGN KEY(book_id) REFERENCES book(book_id),
-    genre_id INTEGER NOT NULL,
-    FOREIGN KEY(genre_id) REFERENCES genre(genre_id)
+CREATE TABLE book_genre (
+    fk_book_id INTEGER NOT NULL,
+    fk_genre_id INTEGER NOT NULL,
+    FOREIGN KEY (fk_book_id) REFERENCES book (book_id),
+    FOREIGN KEY (fk_genre_id) REFERENCES genre (genre_id)
 );
