c6cfe095f6a66bbe94cdfc4bb7b4302819570ff0
Alex Curran <alexcurran.va@gmail.com>
2024-01-31 15:35:24 -0600

    Add sql schema file with tables for book and author

---
+++ db/books_db_schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE book(
+    book_id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    year INTEGER NOT NULL,
+    pages INTEGER NOT NULL,
+    author_id INTEGER NOT NULL,
+    FOREIGN KEY(author_id) REFERENCES author(author_id)
+);
+
+CREATE TABLE author(
+    author_id INTEGER PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL
+);
