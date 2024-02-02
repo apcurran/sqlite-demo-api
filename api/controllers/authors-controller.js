ccb5c0b79f79c2c8aeb28dd07a643573d1ef50c8
Alex Curran <alexcurran.va@gmail.com>
2024-02-02 16:15:20 -0600

    Add postAuthor controller logic

--- api/controllers/authors-controller.js
+++ api/controllers/authors-controller.js
@@ -1,18 +1,40 @@
 "use strict";
 
-async function getAuthors() {
-    
-}
+const { db } = require("../../db/index");
 
-async function getAuthor() {
-
-}
-
-async function postAuthor() {
+/** @type {import("express").RequestHandler} */
+function getAuthors(req, res, next) {
 
 }
 
-async function deleteAuthor() {
+/** @type {import("express").RequestHandler} */
+function getAuthor(req, res, next) {
+
+}
+
+/** @type {import("express").RequestHandler} */
+function postAuthor(req, res, next) {
+    const { firstName, lastName } = req.body;
+    const sql = `
+            INSERT INTO author
+                (first_name, last_name)
+            VALUES
+                (?, ?);
+        `;
+    const params = [firstName, lastName];
+    db.run(sql, params, function (err) {
+        if (err) {
+            next(err);
+
+            res.status(500).json({ error: "Something went wrong." });
+        }
+
+        res.status(201).json({ msg: "Author added." });
+    });
+}
+
+/** @type {import("express").RequestHandler} */
+function deleteAuthor(req, res, next) {
 
 }
 
