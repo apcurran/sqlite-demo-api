12f5d2d53011370a0a62b3cfed576edc3e07c65d
Alex Curran <alexcurran.va@gmail.com>
2024-02-02 14:03:33 -0600

    Simplfy tables for basic SQLite demo

--- db/books_db_schema.sql
+++ db/books_db_schema.sql
@@ -3,6 +3,7 @@ CREATE TABLE book (
     title TEXT NOT NULL,
     year INTEGER NOT NULL,
     pages INTEGER NOT NULL,
+    genre TEXT NOT NULL,
     author_id INTEGER NOT NULL,
     FOREIGN KEY (author_id) REFERENCES author (author_id)
 );
@@ -11,17 +12,4 @@ CREATE TABLE author (
     author_id INTEGER PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL
-);
-
-CREATE TABLE genre (
-    genre_id INTEGER PRIMARY KEY,
-    genre_name TEXT NOT NULL
-);
-
--- junction table connecting book with genre
-CREATE TABLE book_genre (
-    fk_book_id INTEGER NOT NULL,
-    fk_genre_id INTEGER NOT NULL,
-    FOREIGN KEY (fk_book_id) REFERENCES book (book_id),
-    FOREIGN KEY (fk_genre_id) REFERENCES genre (genre_id)
 );
